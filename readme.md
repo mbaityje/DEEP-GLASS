9eaae4717abd79e5d2946e5dddcc0956078d991b
Marco <mbaityje@kondo.ipht.fr>
2018-01-07 18:38:01 +0100

    Scripts kondo

--- readme.md
+++ readme.md
@@ -1,3 +1,14 @@
+# Samples
+
+Le samples 0 son quelle del primo run.
+
+Le samples 999 son le due samples di prova per vedere se corre e tutto è a posto.
+
+Gli ultimi run sono le sample 1, che sono 168 simulazioni su bruna10 e conv1020relu.
+
+
+
+
 # Possibili Upgrade
 
 - Tutto l'output nello stesso formato testo.
@@ -9,6 +20,7 @@
 - Relegare qualche funzione a moduli esterni (tipo i tempi), e
   accorciare la lunghezza del main
 
+- Gli script per lanciare su kondo dovrebbero controllare che non ci sia un altro script con lo stesso nome. 
 
 # Per linkare con github
 git remote add origin https://github.com/mbaityje/DEEP-GLASS.git
--- scripts/convert_hist-data_corr.sh
+++ scripts/convert_hist-data_corr.sh
@@ -2,30 +2,31 @@
 
 TOTLENGTH_NORM=`echo 10^6|bc`
 periods=1000
-INIT_DISTROS="normal uniform uniform1"
+INIT_DISTROS="uniform uniform1 normal"
 
 
-for network in bruna10corr
+for network in conv1020relu #bruna10
 do
     for m in 10 100 1000 10000
     do
-	for B in 10 100
+	for B in 1000 100 10
 	do
-	    for LR in 0.5 0.2 0.1 0.01
+	    for LR in 0.01 1.0 0.5 0.2 0.1 0.05 0.02 0.01
 	    do
 		TOTLENGTH=`echo $TOTLENGTH_NORM/$LR|bc`
 		spp=`echo "$TOTLENGTH/($B*$periods)"|bc`
+		echo spp=$spp
 		for wd in 0
 		do
 		    for momentum in 0.5
 		    do
 			for INIT_DISTR in $INIT_DISTROS
 			do
-			    for isam in 0
+			    for isam in 1 0 999
 			    do
 				DIRECTORIO=../output/$network/m$m/B$B/LR$LR/WD$wd/MOM$momentum/$INIT_DISTR/sam$isam
-				FICHERO=$DIRECTORIO/cifar10_0_${B}_bruna10_00000-01000.hist
-				OUTPUT=$DIRECTORIO/cifar10_0_${B}_bruna10_00000-01000.data
+				FICHERO=$DIRECTORIO/cifar10_0_${B}_${network}_00000-01000.hist
+				OUTPUT=$DIRECTORIO/cifar10_0_${B}_${network}_00000-01000.data
 				python convert_hist-data_corr.py $FICHERO $spp > $OUTPUT
 			    done
 			done
